256941ce0eb604eb0040382f65ebdac90d81ae27
Barbosa <joaovictorbarbosa2@gmail.com>
2025-04-23 11:23:45 -0300

    Adicionando mais um exercicio de arrays

---
+++ numeros/Numbers.java
@@ -0,0 +1,8 @@
+public class Numbers{
+	public static void main(String[] args){
+		int[] numbers = new int[]{2,4,6,8};
+		for (int numero : numbers ){
+			System.out.print(numero + " ");
+		}
+ 	}
+}
